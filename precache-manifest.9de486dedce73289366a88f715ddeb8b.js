fd9dba17ac2ac3143fafeea55155d09e2aff9cdc
dklarin <danijel.klarin@gmail.com>
2020-12-01 19:02:31 +0100

    Updates

---
+++ precache-manifest.9de486dedce73289366a88f715ddeb8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b872a344062c7c507df50f0eccf4f7c",
+    "url": "/intentions/index.html"
+  },
+  {
+    "revision": "2a84579aba33758989ba",
+    "url": "/intentions/static/css/2.56e8694c.chunk.css"
+  },
+  {
+    "revision": "61695cedbfe682f37866",
+    "url": "/intentions/static/css/main.84ab6231.chunk.css"
+  },
+  {
+    "revision": "2a84579aba33758989ba",
+    "url": "/intentions/static/js/2.4f344915.chunk.js"
+  },
+  {
+    "revision": "712aed4428eb9f1267a64d2058114953",
+    "url": "/intentions/static/js/2.4f344915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61695cedbfe682f37866",
+    "url": "/intentions/static/js/main.b36e010d.chunk.js"
+  },
+  {
+    "revision": "f17654e6e9323d0e9ec1",
+    "url": "/intentions/static/js/runtime-main.438c7d1a.js"
+  },
+  {
+    "revision": "114592f29d7630ac399840270a584961",
+    "url": "/intentions/static/media/calendar.114592f2.svg"
+  },
+  {
+    "revision": "5e75c209c7f0a5ed6b3a2770ddef732a",
+    "url": "/intentions/static/media/down-arrow.5e75c209.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/intentions/static/media/search.2ea4c436.svg"
+  },
+  {
+    "revision": "b5cd24f24e481eb5246c5f1a6bc77bea",
+    "url": "/intentions/static/media/up-arrow.b5cd24f2.svg"
+  }
+]);
